2514d2728fb006c86539f752416385b5bd6a41ee
mindfreakzerox <mindfreakzerox@users.noreply.github.com>
2026-02-13 21:58:48 +0000

    chore: remove static export config for vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH || '';
-const assetPrefix = process.env.ASSET_PREFIX || basePath;
-
 const nextConfig = {
-  output: 'export',
-  basePath,
-  assetPrefix,
-  trailingSlash: true,
   experimental: {
     webpackBuildWorker: false
   },
